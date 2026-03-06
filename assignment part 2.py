cfc3e500e2a9daab48b7a55e62c68769290b1881
toheed787 <bali55323@gmail.com>
2026-03-06 19:28:59 +0500

    Assignment part 2

---
+++ assignment part 2.py
@@ -0,0 +1,45 @@
+age = 19
+height = 5.9
+print(type(age))
+print(type(height))
+
+#print variable and type
+a = 3 + 4j
+print(a)
+print (type(a))
+
+#boolean true
+Is_Python_fun = True
+print(Is_Python_fun)
+
+#three variables
+a = 1
+b = 2
+c = 3
+print(a,b,c)
+
+#same variable
+a = b = c = 100
+print(a,b,c)
+
+#Numeric value into float
+num = float(input("Enter a number"))
+print(num)
+
+#String input 100
+s = 100
+num = int(s)
+print(num, type(num))
+
+# variable and complex
+
+#paragraph length
+p = "My name is Toheed I am doing data science course"
+print("Length:", len(p))
+
+#Swap variables
+a = 50
+b = 40
+a, b = b, a
+print("a:",a,"b:",b)
+

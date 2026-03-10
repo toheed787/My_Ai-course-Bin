433ab016559f8066b2122e2e29362e3a6d91de91
toheed787 <bali55323@gmail.com>
2026-03-10 22:39:33 +0500

    Assignment part 5
    week 1

---
+++ Assignment part 5.py
@@ -0,0 +1,51 @@
+# += operator
+x = 10
+x += 5
+print(x)
+
+#-= operator
+price = 20
+price -= 3
+print(price)
+
+#*= operator
+a = 50
+a *= 2
+print(a)
+
+#/= operator
+total = 40
+total /= 4
+print(total)
+
+#**= square
+num = 5
+num **= 2
+print(num)
+
+# Counter increment
+counter = 0
+counter += 1
+print(counter)
+
+#%= operator
+num = 10
+num %= 2
+print(num)
+
+#//= operator
+num = 10
+num //= 3
+print(num)
+
+# Convert n = 2 to 20
+n = 2
+n *= 5
+n += 5
+n *= 2
+print(n)
+
+#User input and += 10
+num = int(input("Enter a number "))
+num += 10
+print("Final result", num)
